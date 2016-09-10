1791e5543905cce30cd11c8801c70236806bad51
Christophe Van Neste <christophe.vanneste@ugent.be>
2016-09-10 11:38:51 +0200

    herfst2016

---
+++ js/main.js
@@ -0,0 +1,31 @@
+var renstudioApp = angular.module('renstudioApp', []);
+
+renstudioApp.controller('RenstudioActivities', function RenstudioActivities($scope) {
+    $scope.events = [
+	{
+	    title: 'Rivier',
+	    message: 'Nog plaatsen beschikbaar',
+	    date: 'Zondag 25/09/2016',
+	    time: '14-18u',
+	    image: '37.jpg'
+	}, {
+	    title: 'Bos bij zonsondergang',
+	    message: 'Nog plaatsen beschikbaar',
+	    date: 'Zondag 23/10/2016',
+	    time: '14-18u',
+	    image: '16.jpg'	    
+	}, {
+	    title: 'Lotus',
+	    message: 'Nog plaatsen beschikbaar',
+	    date: 'Zondag 27/11/2016',
+	    time: '14-18u',
+	    image: '52.jpg'	    
+	}, {
+	    title: 'Sneeuwlandschap',
+	    message: 'Nog plaatsen beschikbaar',
+	    date: 'Zondag 18/12/2016',
+	    time: '14-18u',
+	    image: '43.jpg'	    
+	}
+  ];
+});
