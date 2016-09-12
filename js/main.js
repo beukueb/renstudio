6bbeb0933ff912c040cd62df6233281afd1ca42f
Christophe Van Neste <christophe.vanneste@ugent.be>
2016-09-12 21:10:36 +0200

    firebase up and running

--- js/main.js
+++ js/main.js
@@ -3,33 +3,4 @@ var renstudioApp = angular.module('renstudioApp', ['firebase']);
 renstudioApp.controller('RenstudioActivities', function RenstudioActivities($scope,$firebaseArray) {
     var ref = firebase.database().ref().child("events");
     $scope.events = $firebaseArray(ref);
-    
-    //$scope.events = $scope.events.concat([
-    /*$scope.events = [
-	{
-	    title: 'Rivier',
-	    message: 'Nog plaatsen beschikbaar',
-	    date: 'Zondag 25/09/2016',
-	    time: '14-18u',
-	    image: '37.jpg'
-	}, {
-	    title: 'Bos bij zonsondergang',
-	    message: 'Nog plaatsen beschikbaar',
-	    date: 'Zondag 23/10/2016',
-	    time: '14-18u',
-	    image: '16.jpg'	    
-	}, {
-	    title: 'Lotus',
-	    message: 'Nog plaatsen beschikbaar',
-	    date: 'Zondag 27/11/2016',
-	    time: '14-18u',
-	    image: '52.jpg'	    
-	}, {
-	    title: 'Sneeuwlandschap',
-	    message: 'Nog plaatsen beschikbaar',
-	    date: 'Zondag 18/12/2016',
-	    time: '14-18u',
-	    image: '43.jpg'	    
-	}
-    ];*/
 });
