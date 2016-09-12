0e185a52119d73a9db1ce2f15c1b2bd682226ec2
Christophe Van Neste <christophe.vanneste@ugent.be>
2016-09-12 20:50:39 +0200

    firebaseArray

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var renstudioApp = angular.module('renstudioApp', ['firebase']);
 
-renstudioApp.controller('RenstudioActivities', function RenstudioActivities($scope,$firebaseObject) {
+renstudioApp.controller('RenstudioActivities', function RenstudioActivities($scope,$firebaseArray) {
     var ref = firebase.database().ref().child("events");
     $scope.events = $firebaseArray(ref);
     
