7f150fc95075c76ccc2c8f3cbb1f41d8e88dd4a8
Christophe Van Neste <christophe.vanneste@ugent.be>
2016-09-12 20:49:10 +0200

    firebase most recent libs

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 var renstudioApp = angular.module('renstudioApp', ['firebase']);
 
 renstudioApp.controller('RenstudioActivities', function RenstudioActivities($scope,$firebaseObject) {
-    var ref = new Firebase("https://rybina-a22ae.firebaseio.com/events");
-    //$scope.events = $firebaseObject(ref);
+    var ref = firebase.database().ref().child("events");
+    $scope.events = $firebaseArray(ref);
     
     //$scope.events = $scope.events.concat([
-    $scope.events = [
+    /*$scope.events = [
 	{
 	    title: 'Rivier',
 	    message: 'Nog plaatsen beschikbaar',
@@ -31,5 +31,5 @@ renstudioApp.controller('RenstudioActivities', function RenstudioActivities($sco
 	    time: '14-18u',
 	    image: '43.jpg'	    
 	}
-    ];
+    ];*/
 });
